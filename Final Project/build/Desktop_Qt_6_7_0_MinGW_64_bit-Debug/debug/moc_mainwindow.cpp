156dfa9d16da45f732295224241a89dbc694eaa5
ahmed <ahmedehab5750@gmail.com>
2024-04-15 09:04:48 +0200

    Done The Gui

---
+++ Final Project/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,336 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../mainwindow.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "on_Grey_f1_clicked",
+    "",
+    "on_Black_White_f2_clicked",
+    "on_Invert_f3_clicked",
+    "on_Merge_f4_clicked",
+    "on_Flip_f5_clicked",
+    "on_Rotate_f6_clicked",
+    "on_Brightness_f7_clicked",
+    "on_Crop_f8_clicked",
+    "on_Frame_f9_clicked",
+    "on_Edges_f10_clicked",
+    "on_Resize_f11_clicked",
+    "on_Blur_f12_clicked",
+    "on_Sun_f13_clicked",
+    "on_OilPainting_f14_clicked",
+    "on_TV_f15_clicked",
+    "on_Purple_f16_clicked",
+    "on_InfraRed_f17_clicked",
+    "on_Skew_f18_clicked",
+    "on_WhiteRed_e1_clicked",
+    "on_Blood_e2_clicked",
+    "on_RedGreen_e3_clicked",
+    "on_XRay_e4_clicked",
+    "on_LoadImage_clicked",
+    "on_SaveImage_clicked",
+    "on_ResetEffects_clicked",
+    "on_MergeC1_clicked",
+    "on_MergeC2_clicked",
+    "on_FlipH_clicked",
+    "on_FlipV_clicked",
+    "on_BrightSl_sliderReleased",
+    "on_RotateL_clicked",
+    "on_RotateR_clicked",
+    "on_FrameR_clicked",
+    "on_FrameB_clicked",
+    "on_FrameG_clicked",
+    "on_FrameBW_clicked"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      36,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  230,    2, 0x08,    1 /* Private */,
+       3,    0,  231,    2, 0x08,    2 /* Private */,
+       4,    0,  232,    2, 0x08,    3 /* Private */,
+       5,    0,  233,    2, 0x08,    4 /* Private */,
+       6,    0,  234,    2, 0x08,    5 /* Private */,
+       7,    0,  235,    2, 0x08,    6 /* Private */,
+       8,    0,  236,    2, 0x08,    7 /* Private */,
+       9,    0,  237,    2, 0x08,    8 /* Private */,
+      10,    0,  238,    2, 0x08,    9 /* Private */,
+      11,    0,  239,    2, 0x08,   10 /* Private */,
+      12,    0,  240,    2, 0x08,   11 /* Private */,
+      13,    0,  241,    2, 0x08,   12 /* Private */,
+      14,    0,  242,    2, 0x08,   13 /* Private */,
+      15,    0,  243,    2, 0x08,   14 /* Private */,
+      16,    0,  244,    2, 0x08,   15 /* Private */,
+      17,    0,  245,    2, 0x08,   16 /* Private */,
+      18,    0,  246,    2, 0x08,   17 /* Private */,
+      19,    0,  247,    2, 0x08,   18 /* Private */,
+      20,    0,  248,    2, 0x08,   19 /* Private */,
+      21,    0,  249,    2, 0x08,   20 /* Private */,
+      22,    0,  250,    2, 0x08,   21 /* Private */,
+      23,    0,  251,    2, 0x08,   22 /* Private */,
+      24,    0,  252,    2, 0x08,   23 /* Private */,
+      25,    0,  253,    2, 0x08,   24 /* Private */,
+      26,    0,  254,    2, 0x08,   25 /* Private */,
+      27,    0,  255,    2, 0x08,   26 /* Private */,
+      28,    0,  256,    2, 0x08,   27 /* Private */,
+      29,    0,  257,    2, 0x08,   28 /* Private */,
+      30,    0,  258,    2, 0x08,   29 /* Private */,
+      31,    0,  259,    2, 0x08,   30 /* Private */,
+      32,    0,  260,    2, 0x08,   31 /* Private */,
+      33,    0,  261,    2, 0x08,   32 /* Private */,
+      34,    0,  262,    2, 0x08,   33 /* Private */,
+      35,    0,  263,    2, 0x08,   34 /* Private */,
+      36,    0,  264,    2, 0x08,   35 /* Private */,
+      37,    0,  265,    2, 0x08,   36 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_Grey_f1_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Black_White_f2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Invert_f3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Merge_f4_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Flip_f5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Rotate_f6_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Brightness_f7_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Crop_f8_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Frame_f9_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Edges_f10_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Resize_f11_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Blur_f12_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Sun_f13_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_OilPainting_f14_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_TV_f15_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Purple_f16_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_InfraRed_f17_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Skew_f18_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_WhiteRed_e1_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Blood_e2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_RedGreen_e3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_XRay_e4_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_LoadImage_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_SaveImage_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_ResetEffects_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_MergeC1_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_MergeC2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_FlipH_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_FlipV_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BrightSl_sliderReleased'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_RotateL_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_RotateR_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_FrameR_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_FrameB_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_FrameG_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_FrameBW_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_Grey_f1_clicked(); break;
+        case 1: _t->on_Black_White_f2_clicked(); break;
+        case 2: _t->on_Invert_f3_clicked(); break;
+        case 3: _t->on_Merge_f4_clicked(); break;
+        case 4: _t->on_Flip_f5_clicked(); break;
+        case 5: _t->on_Rotate_f6_clicked(); break;
+        case 6: _t->on_Brightness_f7_clicked(); break;
+        case 7: _t->on_Crop_f8_clicked(); break;
+        case 8: _t->on_Frame_f9_clicked(); break;
+        case 9: _t->on_Edges_f10_clicked(); break;
+        case 10: _t->on_Resize_f11_clicked(); break;
+        case 11: _t->on_Blur_f12_clicked(); break;
+        case 12: _t->on_Sun_f13_clicked(); break;
+        case 13: _t->on_OilPainting_f14_clicked(); break;
+        case 14: _t->on_TV_f15_clicked(); break;
+        case 15: _t->on_Purple_f16_clicked(); break;
+        case 16: _t->on_InfraRed_f17_clicked(); break;
+        case 17: _t->on_Skew_f18_clicked(); break;
+        case 18: _t->on_WhiteRed_e1_clicked(); break;
+        case 19: _t->on_Blood_e2_clicked(); break;
+        case 20: _t->on_RedGreen_e3_clicked(); break;
+        case 21: _t->on_XRay_e4_clicked(); break;
+        case 22: _t->on_LoadImage_clicked(); break;
+        case 23: _t->on_SaveImage_clicked(); break;
+        case 24: _t->on_ResetEffects_clicked(); break;
+        case 25: _t->on_MergeC1_clicked(); break;
+        case 26: _t->on_MergeC2_clicked(); break;
+        case 27: _t->on_FlipH_clicked(); break;
+        case 28: _t->on_FlipV_clicked(); break;
+        case 29: _t->on_BrightSl_sliderReleased(); break;
+        case 30: _t->on_RotateL_clicked(); break;
+        case 31: _t->on_RotateR_clicked(); break;
+        case 32: _t->on_FrameR_clicked(); break;
+        case 33: _t->on_FrameB_clicked(); break;
+        case 34: _t->on_FrameG_clicked(); break;
+        case 35: _t->on_FrameBW_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 36)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 36;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 36)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 36;
+    }
+    return _id;
+}
+QT_WARNING_POP
---
+++ Final Project/mainwindow.h
@@ -0,0 +1,104 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    QString name1, name2;
+private slots:
+
+    void on_Grey_f1_clicked();
+
+    void on_Black_White_f2_clicked();
+
+    void on_Invert_f3_clicked();
+
+    void on_Merge_f4_clicked();
+
+    void on_Flip_f5_clicked();
+
+    void on_Rotate_f6_clicked();
+
+    void on_Brightness_f7_clicked();
+
+    void on_Crop_f8_clicked();
+
+    void on_Frame_f9_clicked();
+
+    void on_Edges_f10_clicked();
+
+    void on_Resize_f11_clicked();
+
+    void on_Blur_f12_clicked();
+
+    void on_Sun_f13_clicked();
+
+    void on_OilPainting_f14_clicked();
+
+    void on_TV_f15_clicked();
+
+    void on_Purple_f16_clicked();
+
+    void on_InfraRed_f17_clicked();
+
+    void on_Skew_f18_clicked();
+
+    void on_WhiteRed_e1_clicked();
+
+    void on_Blood_e2_clicked();
+
+    void on_RedGreen_e3_clicked();
+
+    void on_XRay_e4_clicked();
+
+    void on_LoadImage_clicked();
+
+    void on_SaveImage_clicked();
+
+    void on_ResetEffects_clicked();
+
+    void on_MergeC1_clicked();
+
+    void on_MergeC2_clicked();
+
+    void on_FlipH_clicked();
+
+    void on_FlipV_clicked();
+
+    void on_BrightSl_sliderReleased();
+
+    void on_RotateL_clicked();
+
+    void on_RotateR_clicked();
+
+    void on_FrameR_clicked();
+
+    void on_FrameB_clicked();
+
+    void on_FrameG_clicked();
+
+    void on_FrameBW_clicked();
+
+    void on_BlurSl_sliderReleased();
+
+    void on_SkewSl_sliderReleased();
+
+    void on_OilFast_clicked();
+
+    void on_OilSlow_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
